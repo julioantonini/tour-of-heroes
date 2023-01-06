d8e4015b96a52dfc1f6386c9d1385d37f4e2c3ea
Julio Antonini <julio.antonini@gylabs.com.br>
2023-01-06 20:31:15 -0300

    feat: create hero detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,5 +9,10 @@ import { Hero } from '../hero.model';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
-  hero: Hero = { id: 1, name: 'hero' };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
