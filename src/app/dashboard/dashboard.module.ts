4c9949ba7dfb63ca389ae6a4d4d6f72b53684f61
Julio Antonini <julio.antonini@gylabs.com.br>
2023-01-14 16:41:25 -0300

    feat: create dashboard and heroes module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { MaterialModule } from '../material/material.module';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, MaterialModule, AppRoutingModule],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    AppRoutingModule,
+    DashboardRoutingModule,
+  ],
 })
 export class DashboardModule {}
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,14 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { MaterialModule } from '../material/material.module';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
 
 @NgModule({
   declarations: [HeroesComponent, HeroDetailComponent],
-  imports: [CommonModule, FormsModule, MaterialModule, RouterModule],
+  imports: [CommonModule, FormsModule, MaterialModule, HeroesRoutingModule],
 })
 export class HeroesModule {}
